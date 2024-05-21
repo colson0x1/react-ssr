34ee850e3a2b0b6065c0759c6d0067373f2de86e
Colson <colsoncalifornication@gmail.com>
2024-05-21 16:48:49 +0545

    convert home component into HTML and render in express server with the ReactDOM renderToString fn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 // CommonJS module syntax or the `require` syntax
 
+/* @ ReactDOM */
+// ReactDOM library has `render` and also the `renderToString` fn
+// `render` fn creates instances of a bunch of components and mounts them to a
+// DOM node
+// `renderToString` renders a bunch of components one time and produces a string
+// out of all the resulting HTML. So rather than mounting to some DOM node,
+// instead renders all those components to exactly one time, converts the
+// output of them to raw HTML and returns it as a string.
+
 const express = require('express');
+// Making ES2015 modules (React, renderToStrinf, Home) works nicely
+// with CommonJS module system
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString();
+const Home = require('./client/components/Home').default;
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+
+  res.send(content);
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
