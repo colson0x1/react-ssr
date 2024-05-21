eff2f5b198636e35e58730333736e5de38f782cc
Colson <colsoncalifornication@gmail.com>
2024-05-21 23:54:17 +0545

    refactor require statements to the ES 2015 module system undernath all the existing require statements

--- build/bundle.js
+++ build/bundle.js
@@ -7042,10 +7042,73 @@ if (process.env.NODE_ENV === 'production') {
 "use strict";
 
 
+var _express = __webpack_require__(54);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(16);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(110);
+
+var _Home = __webpack_require__(121);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 // React SSR
 /* @ Server Side Rendering (SSR)
- * @ Isomorphic React
+ * - Generates HTML on the server
+ * @ Server Side Templating (EJS)
+ * - Generates HTML on the server
+ *
+ * SSR refers to our ability to somehow produce HTML on our server side and
+ * ship it down to the user's browser.
+ * The is kind of misleading terminology because technically we're doing
+ * server side rendering even without React, or we can do server side rendering
+ * without React. We could use HTML templates on the server like EJS, Handlebars
+ * or Pug and that's really still server side rendering! So we could also refer
+ * to that as server side templating.
+ *
+ * Some people decided that the term Server Side Rendering wasn't specific enough,
+ * so they came up with two other terms.
  * @ Universal JavaScript
+ * - The same code runs on the server and the browser
+ * @ Isomorphic JavaScript
+ * - The same code runs on the server and the browser
+ *
+ * So those two terms were Universal JavaScript and Isomorphic JavaScript.
+ * Both these terms essentially mean that some amount of code that we're writing
+ * on the server might also be executed on the browser, and the opposite is
+ * true as well. So code that is intended for the browser might be executed
+ * on the server.
+ * There is one specific thing inside of our app that kind of flies right in the
+ * face of this whole idea of Universal JavaScript or Isomorphic Javascript
+ * and that is the two different module systems. So inside of index.js, we're
+ * making use of CommonJS module system that is characterized by require
+ * statements. But inside of our Home Component, we're using ES2015 Modules,
+ * which is a distinctly different module system.
+ * Part of working with Isomorphic or Universal JavaScript means that, we should
+ * be able to use the exact same coding style on both our client side and
+ * server side!
+ * Normally we need to use CommonJS for NODE because no ES2015 Module was
+ * supported for NODE until few years ago.
+ * However, running Webpack and Babel over our entire codebase which includes
+ * the NODEjs inside of this index.js file could make ES2015 Module system
+ * work in NODEjs even before it was officially supported.
+ * So because our server files are being handled by Webpack, we can make use of
+ * ES2015 Modules on our Server Side code as well.
+ * And we want to do that specifically to kind of lessen the distinction between
+ * our server side code and and client side code.
+ * In other words, as Engineers, as we're working on these projects, we don't
+ * want to have to do a big context shift as we start working on our React
+ * code versus Express code. We want to be able to write the exact same
+ * dialect of JavaScript on both sides.
+ *
+ * @ Isomorphic React
+ * - The same code runs on the server and the browser
  **/
 
 // CommonJS module syntax or the `require` syntax
@@ -7090,16 +7153,25 @@ if (process.env.NODE_ENV === 'production') {
 // This renders all of our React Components exactly one time, builds an HTML
 // strucuture out of them and then sends the result down to our browser.
 
-var express = __webpack_require__(54);
+// Webpack allows us to use ES2015 modules on our front end code.
+// So because our server files are being handled by Webpack, we can now make
+// use of ES2015 modules on our server side code as well.
+// We want to do that to use the exact same coding style on both the React
+// specific code and on our Server Side code (Express) as well
+
+/*
+const express = require('express');
 // Making ES2015 modules (React, renderToStrinf, Home) works nicely
 // with CommonJS module system
-var React = __webpack_require__(16);
-var renderToString = __webpack_require__(110).renderToString;
-var Home = __webpack_require__(121).default;
-var app = express();
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+*/
+
+var app = (0, _express2.default)();
 
 app.get('/', function (req, res) {
-  var content = renderToString(React.createElement(Home, null));
+  var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
   res.send(content);
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,54 @@
 // React SSR
 /* @ Server Side Rendering (SSR)
- * @ Isomorphic React
+ * - Generates HTML on the server
+ * @ Server Side Templating (EJS)
+ * - Generates HTML on the server
+ *
+ * SSR refers to our ability to somehow produce HTML on our server side and
+ * ship it down to the user's browser.
+ * The is kind of misleading terminology because technically we're doing
+ * server side rendering even without React, or we can do server side rendering
+ * without React. We could use HTML templates on the server like EJS, Handlebars
+ * or Pug and that's really still server side rendering! So we could also refer
+ * to that as server side templating.
+ *
+ * Some people decided that the term Server Side Rendering wasn't specific enough,
+ * so they came up with two other terms.
  * @ Universal JavaScript
+ * - The same code runs on the server and the browser
+ * @ Isomorphic JavaScript
+ * - The same code runs on the server and the browser
+ *
+ * So those two terms were Universal JavaScript and Isomorphic JavaScript.
+ * Both these terms essentially mean that some amount of code that we're writing
+ * on the server might also be executed on the browser, and the opposite is
+ * true as well. So code that is intended for the browser might be executed
+ * on the server.
+ * There is one specific thing inside of our app that kind of flies right in the
+ * face of this whole idea of Universal JavaScript or Isomorphic Javascript
+ * and that is the two different module systems. So inside of index.js, we're
+ * making use of CommonJS module system that is characterized by require
+ * statements. But inside of our Home Component, we're using ES2015 Modules,
+ * which is a distinctly different module system.
+ * Part of working with Isomorphic or Universal JavaScript means that, we should
+ * be able to use the exact same coding style on both our client side and
+ * server side!
+ * Normally we need to use CommonJS for NODE because no ES2015 Module was
+ * supported for NODE until few years ago.
+ * However, running Webpack and Babel over our entire codebase which includes
+ * the NODEjs inside of this index.js file could make ES2015 Module system
+ * work in NODEjs even before it was officially supported.
+ * So because our server files are being handled by Webpack, we can make use of
+ * ES2015 Modules on our Server Side code as well.
+ * And we want to do that specifically to kind of lessen the distinction between
+ * our server side code and and client side code.
+ * In other words, as Engineers, as we're working on these projects, we don't
+ * want to have to do a big context shift as we start working on our React
+ * code versus Express code. We want to be able to write the exact same
+ * dialect of JavaScript on both sides.
+ *
+ * @ Isomorphic React
+ * - The same code runs on the server and the browser
  **/
 
 // CommonJS module syntax or the `require` syntax
@@ -46,12 +93,26 @@
 // This renders all of our React Components exactly one time, builds an HTML
 // strucuture out of them and then sends the result down to our browser.
 
+// Webpack allows us to use ES2015 modules on our front end code.
+// So because our server files are being handled by Webpack, we can now make
+// use of ES2015 modules on our server side code as well.
+// We want to do that to use the exact same coding style on both the React
+// specific code and on our Server Side code (Express) as well
+
+/*
 const express = require('express');
 // Making ES2015 modules (React, renderToStrinf, Home) works nicely
 // with CommonJS module system
 const React = require('react');
 const renderToString = require('react-dom/server').renderToString;
 const Home = require('./client/components/Home').default;
+*/
+
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Home from './client/components/Home';
+
 const app = express();
 
 app.get('/', (req, res) => {
