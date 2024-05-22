c37a7f6bb7a8ce1bdf13b7b38a197612a5b000ec
Colson <colsoncalifornication@gmail.com>
2024-05-22 08:08:04 +0545

    pain point: only raw html content is returned from the server but No client side js like React. example - js code like event handlers are never executed

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,9 +1,42 @@
 // ES2015 Modules syntax or the `import .. export` syntax
 
+/* @ Normal React Application
+ * In a normal traditional React application, we would have a JavaScript file
+ * that gets loaded into the browser and that then gets executed. The JS file
+ * would render our JavaScript application, stick it into the DOM and then
+ * attach any related event handlers that we set up inside of the code base.
+ * So with the normal application, we ship down our entire JavaScript bundle
+ * file to the browser and that renders the app and sets up event handlers
+ * inside the browser.
+ *
+ * With this current setup, there's no JavaScript code being set down to the
+ * users browser right now.
+ * We make a request to the root route, the express server sends back the HTML
+ * from that Home component and absolutely nothing else. There's no JS code
+ * that is being loaded into the browser that sets up that event handler for us.
+ * We could check that on the network log in response.
+ * So in order to actually make sure that we get some JavaScript or have our
+ * application work correctly, we need to make sure that we somehow ship down
+ * all the JavaScript code related to our application after we ship down all
+ * this HTML that gets some initial content on the screen.
+ *
+ * So right now in the Server Side world, we are taking care of step number one.
+ * Step number one is getting HTML or getting content to show up on the screen.
+ * Step number two is, however, is to make sure that we then load up our
+ * React application and have the React application set up all the event handlers
+ * and action creators and data loading requests and all that kind of stuff that
+ * we normally want to have occur inside of our application.
+ */
+
 import React from 'react';
 
 const Home = () => {
-  return <div style={{ color: 'dodgerblue' }}>Home Component!!! </div>;
+  return (
+    <div style={{ color: 'dodgerblue' }}>
+      Home Component!!!
+      <button onClick={() => console.log('Hi there!')}>Press me!</button>
+    </div>
+  );
 };
 
 export default Home;
