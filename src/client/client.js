6f69c551233e394373ce28f3c52bbd07209eada3
Colson <colsoncalifornication@gmail.com>
2024-05-22 16:36:54 +0545

    client bootup: hydration - boot up location of our app in the browser: render the app once on the server and then breathe life into it (load React and ReactDOM) on the browser i.e Hydrate initial renered HTML with React

--- src/client/client.js
+++ src/client/client.js
@@ -3,4 +3,25 @@
 
 // @ Startup point for the client side application
 
-console.log('Hi there!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+// Really important distinction here is that, When this code right here is
+// executed on the browser side, there is already content inside of that div
+// with id of `root`, all the content that we had already rendered into it
+// from the server.
+// So when we call ReactDOM.render(), we are not replacing all the HTML inside
+// there. We are telling React to go back through and set up all those event
+// handlers or all the necessary code that needs to be executed to kind of
+// sort of bind to that existing structure that's on the page!
+
+// This entire process of kind of putting functionality back into the DOM that
+// was already rendered or all the HTML is already rendered, is reffered to as
+// Hydration and if we look at that warning on the console, it actually makes
+// mention of hydate. So the process of kind of rerendering over the once
+// rendered HTML is reffered to as Hydration.
+/* ReactDOM.render(<Home />, document.querySelector('#root')); */
+// Resolve that warning with ReactDOM.hydate to rehydrate our application on the
+// browser.
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
