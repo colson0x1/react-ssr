bf8c4bbe3a7bb84e23e72cbfc3a622a4eb3403f2
Colson <colsoncalifornication@gmail.com>
2024-05-23 15:23:37 +0545

    create fetch users action creator

---
+++ src/client/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// Fetch list of users
+export const FETCH_USERS = 'fetch_users';
+
+export const fetchUsers = () => async (dispatch) => {
+  const res = await axios.get('http://react-ssr-api.herokuapp.com/users');
+
+  dispatch({
+    type: FETCH_USERS,
+    payload: res,
+  });
+};
