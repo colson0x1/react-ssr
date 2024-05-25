288adfbd0e9b5513b8f2c202f24199b60c2c821f
Colson <colsoncalifornication@gmail.com>
2024-05-25 14:19:12 +0545

    fetch auth piece of state through connect function and the mapStateTOProps function in Header

--- public/bundle.js
+++ public/bundle.js
@@ -40103,9 +40103,16 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactRouterDom = __webpack_require__(30);
 
+var _reactRedux = __webpack_require__(105);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function () {
+// Now Header component should be receiving a prop of auth
+var Header = function Header(_ref) {
+  var auth = _ref.auth;
+
+  console.log('My auth status is', auth);
+
   return _react2.default.createElement(
     'div',
     null,
@@ -40116,6 +40123,14 @@ exports.default = function () {
     )
   );
 };
+
+function mapStateToProps(_ref2) {
+  var auth = _ref2.auth;
+
+  return { auth: auth };
+}
+
+exports.default = (0, _reactRedux.connect)(mapStateToProps)(Header);
 
 /***/ }),
 /* 489 */
--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default () => {
+// Now Header component should be receiving a prop of auth
+const Header = ({ auth }) => {
+  console.log('My auth status is', auth);
+
   return (
     <div>
       <Link to='/'>React SSR</Link>
     </div>
   );
 };
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
